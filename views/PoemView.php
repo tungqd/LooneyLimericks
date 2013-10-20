4e5289c28ee6ba19893c95c7151e50967aaf5c0e
khanhnguyen38 <katenguyenvn@Khanhs-MacBook-Pro.local>
2013-10-19 23:33:49 -0700

    Added Javascript func for Rating

--- views/PoemView.php
+++ views/PoemView.php
@@ -35,11 +35,87 @@ class PoemView extends BaseView
 		</div>
 		
 		<div id="rate">
-			<img id="1" src="nostar.png" onmouseover="glow(this.id)" onmouseout="hide(this.id)" />
-			<img id="2" src="nostar.png" onmouseover="glow(this.id)" onmouseout="hide(this.id)" />
-			<img id="3" src="nostar.png" onmouseover="glow(this.id)" onmouseout="hide(this.id)" />
-			<img id="4" src="nostar.png" onmouseover="glow(this.id)" onmouseout="hide(this.id)" />
-			<img id="5" src="nostar.png" onmouseover="glow(this.id)" onmouseout="hide(this.id)" />
+			<img id="1" onclick="rate(this.id)" onmouseover="highlight(this.id)" onmouseout="clear(this.id)" src="grayStar.png" />
+			<img id="2" onclick="rate(this.id)" onmouseover="highlight(this.id)" onmouseout="clear(this.id)" src="grayStar.png" />
+			<img id="3" onclick="rate(this.id)" onmouseover="highlight(this.id)" onmouseout="clear(this.id)" src="grayStar.png" />
+			<img id="4" onclick="rate(this.id)" onmouseover="highlight(this.id)" onmouseout="clear(this.id)" src="grayStar.png" />
+			<img id="5" onclick="rate(this.id)" onmouseover="highlight(this.id)" onmouseout="clear(this.id)" src="grayStar.png" />
+			
+			<script>
+				function highlight(id)
+				{
+					document.getElementById(id).src="fullstar.png";
+					if(id==2)
+					{
+						highlight(1);
+					}
+					else if(id==3)
+					{
+						highlight(1);
+						highlight(2);
+					}
+					else if(id==4)
+					{
+						highlight(1);
+						highlight(2);
+						highlight(3);
+					}
+					else if(id==5)
+					{
+						highlight(1);
+						highlight(2);
+						highlight(3);
+						highlight(4);
+					}
+				}
+
+				function clear(id)
+				{
+					if(document.getElementById("rating").innerHTML==1)
+					{
+						grayOut(2);
+						grayOut(3);
+						grayOut(4);
+						grayOut(5);
+					}
+					else if(document.getElementById("rating").innerHTML==2)
+					{
+						grayOut(3);
+						grayOut(4);
+						grayOut(5);
+					}
+					else if(document.getElementById("rating").innerHTML==3)
+					{
+						grayOut(4);
+						grayOut(5);
+					}
+					else if(document.getElementById("rating").innerHTML==4)
+					{
+						grayOut(5);
+					}
+					else if(document.getElementById("rating").innerHTML==5)
+					{
+					}
+					else
+					{
+						grayOut(1);
+						grayOut(2);
+						grayOut(3);
+						grayOut(4);
+						grayOut(5);
+					}
+				}
+
+				function rate(id)
+				{
+					document.getElementById("rating").innerHTML=id;
+				}
+				
+				function grayOut(id)
+				{
+					document.getElementById(id).src="grayStar.png";
+				}
+			</script>
 		</div>
 	</div>
 </div>
