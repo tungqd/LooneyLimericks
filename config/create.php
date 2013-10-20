d3f911d310c89424ec681a9a885b9d4681eb211d
Tung Dang <tungqd90@yahoo.com>
2013-10-19 18:07:52 -0700

    done with initial database and schemas

--- config/create.php
+++ config/create.php
@@ -13,4 +13,20 @@
  		 echo "Error creating database: " . mysqli_error($db);
  	}
 	
+	$tableQuery = "USE $database;";
+	$tableQuery .= "DROP TABLE IF EXISTS Poem;";
+	$tableQuery .= "CREATE TABLE Poem (ID INT not null,title VARCHAR(30) not null,author VARCHAR(30) not null,content VARCHAR(200) not null,
+        timeSelected TIME,PRIMARY KEY(ID));";
+    $tableQuery .= "DROP TABLE IF EXISTS Rating;";
+    $tableQuery .= "CREATE TABLE Rating (ID INT not null, pID INT not null, rating INT not null, PRIMARY KEY(ID),
+        FOREIGN KEY(pID) REFERENCES Poem(ID) ON UPDATE CASCADE);";
+ 
+    
+  	if (mysqli_multi_query($db,$tableQuery)) {
+  		echo "Schemas for $database created successfully.";
+  	} else {
+ 		 echo "Error creating tables: " . mysqli_error($db);
+ 	}
+
+    
 ?>
