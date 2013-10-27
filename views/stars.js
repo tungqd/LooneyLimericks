57ecfc954594c059c536a5a263da9fa41493bad2
Tung Dang <tungqd90@yahoo.com>
2013-10-27 16:09:26 -0700

    move js to external

---
+++ views/stars.js
@@ -0,0 +1,93 @@
+function grayOut(id)
+{
+    document.getElementById(id).src='./css/grayStar.png';
+}
+/* highlight the stars before current mouse position */
+function highlight(id)
+{
+    document.getElementById(id).src='./css/yellowStar.png';
+    if(id==2)
+    {
+        highlight(1);    
+    }
+    else if(id==3)
+    {
+        highlight(1);
+        highlight(2);
+    }
+    else if(id==4)
+    {
+        highlight(1);
+        highlight(2);
+        highlight(3);
+    }
+    else if(id==5)
+    {
+        highlight(1);
+        highlight(2);
+        highlight(3);
+        highlight(4);
+    }
+}
+
+/* unhighlight all stars locating after mouse click position */
+function unHighLight(id)
+{
+    if(document.getElementById("rating").innerHTML==1)
+    {
+        grayOut(2);
+        grayOut(3);
+        grayOut(4);
+        grayOut(5);
+    }
+    else if(document.getElementById("rating").innerHTML==2)
+    {
+        grayOut(3);
+        grayOut(4);
+        grayOut(5);
+    }
+    else if(document.getElementById("rating").innerHTML==3)
+    {
+        grayOut(4);
+        grayOut(5);
+    }
+    else if(document.getElementById("rating").innerHTML==4)
+    {
+        grayOut(5);
+    }
+    else if(document.getElementById("rating").innerHTML==5)
+    {
+
+    }
+    else /* no mouse click */
+    {
+        grayOut(1);
+        grayOut(2);
+        grayOut(3);
+        grayOut(4);
+        grayOut(5);
+    }
+}
+
+/* Display corresponding rate after mouse click and pass rate to controller */
+function rate(id)
+{
+    document.getElementById("rating").innerHTML=id;
+    //document.forms["rate"].submit();
+    
+    window.location.href = "index.php?c=main&ac=ratePoem&view=LandingView&pid="+ pid +"&stars="+id;
+    /*var xmlhttp;
+    if (window.XMLHttpRequest)
+      {// code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp=new XMLHttpRequest();
+      }
+    else
+      {// code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+    xmlhttp.open("GET","index.php?c=main?view=LandingView?ac=ratePoem?pid=<?php echo $result_array[0];?>?stars=" +id,false);
+    xmlhttp.send();
+  */
+}
+
+
