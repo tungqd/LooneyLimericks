8b280e300de4c2d79decd3f729d82a9e2782ef74
khanhnguyen38 <katenguyenvn@Khanhs-MacBook-Pro.local>
2013-10-28 16:44:29 -0700

    Added second initial poem

--- config/create.php
+++ config/create.php
@@ -19,7 +19,8 @@
     $query .= "DROP TABLE IF EXISTS TimePicked;";
     $query .= "CREATE TABLE TimePicked (poemID INT not null, time DATETIME not null, PRIMARY KEY(poemID));"; 
     $query .= "INSERT INTO TimePicked values (1, NOW());";
-    $query .= "INSERT INTO Poem(title, author,content, timeSelected) VALUES ('Initial Poem', 'First Person','There was an old man from Peru\n who dreamed he was eating his shoe\n He awoke in the night\n with a terrible fright\n and found out that it was quite true.', NOW());";
+    $query .= "INSERT INTO Poem(title, author,content, timeSelected) VALUES ('INITIAL POEM', 'First Person','There was an old man from Peru\n who dreamed he was eating his shoe\n He awoke in the night\n with a terrible fright\n and found out that it was quite true.', NOW());";
+    $query .= "INSERT INTO Poem(title, author,content, timeSelected) VALUES ('SECOND POEM', 'Second Person','There was an old man from Peru\n who dreamed he was eating his shoe\n He awoke in the night\n with a terrible fright\n and found out that it was quite true.', (NOW() + 1));";
 
     if (mysqli_multi_query($db,$query)) {
         echo "Database and Schemas for $database created successfully.";
